11b69e1baee311ff1972ed6d299deeae6608f921
Zevanzeval <sazevalmiel@gmail.com>
2024-01-27 21:23:36 +0000

    Inject the forecast HTML from JavaScript

--- index.js
+++ index.js
@@ -50,7 +50,36 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="row">
+    <div class="col-2">
+      <div class="weather-forecast-date">${day}</div>
+      <img
+        src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-night.png" class ="weather-forecast-icon"
+       
+      />
+      <div class="forecast-temperature">
+        <span class="weather-forecast-temperature-max"> 18°</span>
+        <span class="weather-forecast-temperature-min">12°</span>
+      </div>
+    </div>
+  </div>
+`;
+  });
+
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Odessa");
+displayForecast();
